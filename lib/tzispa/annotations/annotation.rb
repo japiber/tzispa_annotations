ed7c30aef606086a4d3d0408e1e23aa1a51359be
Juan Antonio Piñero <japinero@area-integral.com>
2017-10-03 04:45:20 +0200

    make annotations thread safe

--- lib/tzispa/annotations/annotation.rb
+++ lib/tzispa/annotations/annotation.rb
@@ -2,11 +2,23 @@ module Tzispa
   class Annotation
     attr_accessor :context
 
+    @@annmutext = Mutex.new
+
     def +@
-      self.class.annotations << self
+      self.class.synchronize do
+        self.class.annotations << self
+      end
     end
 
     class << self
+      # rubocop:disable Style/ClassVars
+      def annotations
+        return __annotations_container if @@annmutext.locked?
+        synchronize do
+          __annotations_container
+        end
+      end
+
       def +@
         +new
       end
@@ -15,8 +27,14 @@ module Tzispa
         annotations.pop annotations.size
       end
 
-      def annotations
-        Thread.current[:current_annotations] ||= []
+      def synchronize
+        @@annmutext.synchronize { yield }
+      end
+
+      private
+
+      def __annotations_container
+        @@current_annotations ||= []
       end
     end
   end
